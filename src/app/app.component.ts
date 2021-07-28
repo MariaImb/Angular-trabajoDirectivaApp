600a8c5efc1b57bb8f8ff7d80c164135c1d23dd9
María Imbert <imbert.maria@hotmail.com>
2021-07-28 02:19:02 -0300

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ArrayType } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de usuarios';
+  mensaje="";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  entradas:any;
+  
+
+  constructor(){
+
+    this.entradas=[
+      {titulo:"Python cada día mas presete"},
+      {titulo:"Java presente desde hace 20 años"},
+      {titulo:"JavaScript cada vez mas funcional"},
+      {titulo:"Kotlin potencia para tus apps"},
+      {titulo:"¿Donde quedo Pascal?"},
+    ]
+  }
+
+  RegistrarUsuario(){
+    this.registrado=true;
+    this.mensaje="usuario registrado con exito"
+  }
 }
